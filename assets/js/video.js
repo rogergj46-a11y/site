a6ffd70bb2503f656891be51eee4527acbbabe47
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-02-27 01:02:18 -0600

    .

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,15 +1,8 @@
-// Simple preload removal (optional)
-window.addEventListener('load', () => {
-  document.body.classList.remove('is-preload');
-});
-
 // Video thumbnail click handler
 document.querySelectorAll(".thumb").forEach(t => {
   t.addEventListener("click", () => {
     const id = t.getAttribute("data-video");
-    const main = document.getElementById("mainPlayer");
-    if (main) {
-      main.src = `https://www.youtube.com/embed/${id}`;
-    }
+    document.getElementById("mainPlayer").src =
+      `https://www.youtube.com/embed/${id}`;
   });
 });
