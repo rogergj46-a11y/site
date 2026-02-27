78107e367b2134d258563ad251c48a1823dc95ed
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-02-27 00:21:40 -0600

    .

--- D/assets/js/main.js
+++ D/assets/js/main.js
@@ -210,3 +210,26 @@
 		}
 
 })(jQuery);
+
+
+
+
+
+
+
+
+// Simple preload removal (optional)
+window.addEventListener('load', () => {
+  document.body.classList.remove('is-preload');
+});
+
+// Video thumbnail click handler
+document.querySelectorAll(".thumb").forEach(t => {
+  t.addEventListener("click", () => {
+    const id = t.getAttribute("data-video");
+    const main = document.getElementById("mainPlayer");
+    if (main) {
+      main.src = `https://www.youtube.com/embed/${id}`;
+    }
+  });
+});
